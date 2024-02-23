ac6ce2fdb9698b68cbeb62780c7e93e8373b2048
Daniel Sánchez Pazmiño <dsanchezp998@gmail.com>
2024-02-23 14:05:43 -0800

    add new script

---
+++ scripts/ab_exploratory_analysis.R
@@ -0,0 +1,12 @@
+# R Script: AmericasBarometer Exploratory Analysis
+# POL837 Term Research Paper
+# Simon Fraser University 
+# Daniel Sanchez
+# Spring 2024 
+
+# Preliminaries -----------------------------------------------------------
+
+# Load libraries
+
+library(dplyr)
+library(haven)
