dae995936986747acb7600e378b8f15e0b325991
Daniel Sánchez Pazmiño <dsanchezp998@gmail.com>
2024-02-23 14:47:28 -0800

    ab data cleaning

---
+++ scripts/ab_data_cleaning.R
@@ -0,0 +1,39 @@
+# R Script: AmericasBarometer Data Cleaning
+# POL837 Term Research Paper
+# Simon Fraser University 
+# Daniel Sanchez
+# Spring 2024 
+
+# This script performs data cleaning on the AmericasBarometer data.
+
+# Preliminaries -----------------------------------------------------------
+
+# Load libraries
+
+library(dplyr)
+library(haven)
+library(lubridate)
+
+# Load AmericasBarometer (AB) data
+
+# Merged Ecuador file (2004-2023)
+
+ecu_ab_raw <- read_sav("data/ECU_merge_2004-2023_LAPOP_AmericasBarometer_v1.0_w.sav")
+
+# Data Cleaning (full file 2004-2023) ------------------------------------------------------------
+
+# Apply or remove the labels for relevant variables
+
+ecu_ab <-
+    ecu_ab_raw %>% 
+    mutate(year = zap_labels(year),
+           wave = zap_labels(wave),
+           pais = as_factor(pais))
+
+# Data cleaning (2008-2023) ------------------------------------------------------------
+
+# Select only 2008-2023 and clean the data
+
+ecu_ab_2008_2023 <- 
+    ecu_ab %>% 
+    filter(year >= 2008)
--- scripts/ab_exploratory_analysis.R
+++ scripts/ab_exploratory_analysis.R
@@ -4,9 +4,47 @@
 # Daniel Sanchez
 # Spring 2024 
 
+# This script performs exploratory analysis on the AmericasBarometer data.
+
 # Preliminaries -----------------------------------------------------------
 
 # Load libraries
 
 library(dplyr)
 library(haven)
+
+# Load AmericasBarometer (AB) data 
+
+# Merged Ecuador file (2004-2023)
+
+ecu_ab_raw <- read_sav("data/ECU_merge_2004-2023_LAPOP_AmericasBarometer_v1.0_w.sav")
+
+# Analysis ----------------------------------------------------------------
+
+# Look at survey date
+
+ecu_ab_raw %>%
+  select(fecha) %>%
+  glimpse()
+
+# Group by year and type of answer
+
+ecu_ab_raw %>%
+  group_by(year, fecha) %>%
+  summarise(n = n()) %>%
+  ungroup() 
+
+# Neither 2004 nor 2006 seem to have date information
+
+# Type of year variable
+
+ecu_ab_raw %>%
+  select(year) %>%
+  glimpse()
+
+# Select only 2008-2023
+
+ecu_ab_raw %>%
+    filter(year %in% 2008:2023) %>%
+    select(fecha) %>%
+    glimpse()
