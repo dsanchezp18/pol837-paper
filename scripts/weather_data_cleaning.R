d2f2aef3e768b798ca522d2d62e1cbdffde34051
Daniel Sánchez Pazmiño <dsanchezp998@gmail.com>
2024-02-29 11:49:56 -0800

    cleaning weather the data

--- scripts/weather_data_cleaning.R
+++ scripts/weather_data_cleaning.R
@@ -22,16 +22,37 @@ min_temperature_df <- read_csv("data/weather/min_temperature.csv",
 max_temperature_df <- read_csv("data/weather/max_temperature.csv",
                                  show_col_types = FALSE)
 
-precipitation_df <- read_csv("data/weather/precipitation.csv",
+precipitation_raw <- read_csv("data/weather/precipitation.csv",
                               show_col_types = FALSE)
+
+ecuador_cantons_df <- read_csv("data/other/ecuador_cantons.csv",
+                               show_col_types = FALSE)
 
 # Data Cleaning -----------------------------------------------------------
 
-# Creating the average temperature variable
+# Create a temperature dataframe
+# Creating the average temperature variable and add canton identifiers to the temperature data
 
 temperature_df <- 
   min_temperature_df %>% 
+  relocate(year, .after = canton_id) %>%
   rename(min_temp = value) %>% 
   left_join(max_temperature_df  %>% select(canton_id, date, max_temp = value), by = c("date", "canton_id"))  %>% 
+  left_join(ecuador_cantons_df, by = "canton_id")  %>%
   mutate(spread_temp = max_temp - min_temp,
-         avg_temp = spread_temp/2)
+         avg_temp = spread_temp/2) %>% 
+  relocate(canton_name, prov, .after = canton_id) 
+
+# Create a precipitation dataframe
+
+precipitation_df <- 
+  precipitation_raw %>% 
+  relocate(year, .after = canton_id) %>%
+  left_join(ecuador_cantons_df, by = "canton_id")  %>%
+  relocate(canton_name, prov, .after = canton_id)
+
+# Export the data ---------------------------------------------------------
+
+write_csv(temperature_df, "data/weather/temperature_processed.csv")
+
+write_csv(precipitation_df, "data/weather/precipitation_processed.csv")
